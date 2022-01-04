3354ba34c0bffdfaece8b7876ec0629c190a6da1
shakori999 <shakori999@gmail.com>
2022-01-04 17:07:54 +0300

    add cypress-file-upload to packages-lock

---
+++ client/cypress/integration/authentication.spec.js
@@ -0,0 +1,27 @@
+describe('Authentication', function () {
+  it('Can log in.', function () {
+    cy.visit('/#/log-in');
+    cy.get('input#username').type('gary.cole@example.com');
+    cy.get('input#password').type('pAssw0rd', { log: false });
+    cy.get('button').contains('Log in').click();
+    cy.hash().should('eq', '#/');
+  });
+
+  it('Can sign up.', function () {
+    cy.visit('/#/sign-up');
+    cy.get('input#username').type('gary.cole@example.com');
+    cy.get('input#firstName').type('Gary');
+    cy.get('input#lastName').type('Cole');
+    cy.get('input#password').type('pAssw0rd', { log: false });
+    cy.get('select#group').select('driver');
+    cy.fixture('images/photo.jpg').then(photo => {
+        cy.get('input#photo').upload({
+            fileContent: photo,
+            fileName: 'photo.jpg',
+            mimeType: 'application/json'
+        });
+        });
+    cy.get('button').contains('Sign up').click();
+    cy.hash().should('eq', '#/log-in');
+    });
+});
--- client/cypress/integration/navigation.spec.js
+++ client/cypress/integration/navigation.spec.js
@@ -40,13 +40,3 @@ describe('Navigation', function () {
     cy.hash().should('eq', '#/sign-up');
   });
 });
-
-describe('Authentication', function() {
-    it('Can log in.', function () {
-        cy.visit('/#/log-in');
-        cy.get('input#username').type('gary.cole@example.com');
-        cy.get('input#password').type('pAsswOrd', {log: false });
-        cy.get('button').contains('Log in').click();
-        cy.hash().should('eq', '#/');
-    });    
-});
