37c34ce16537662f18f72265cfe5fe6031ec1d1a
shakori999 <shakori999@gmail.com>
2022-01-04 17:14:58 +0300

    Sign up passed

--- client/cypress/integration/authentication.spec.js
+++ client/cypress/integration/authentication.spec.js
@@ -8,19 +8,14 @@ describe('Authentication', function () {
   });
 
   it('Can sign up.', function () {
+    const filepath = 'images/photo.jpg'
     cy.visit('/#/sign-up');
     cy.get('input#username').type('gary.cole@example.com');
     cy.get('input#firstName').type('Gary');
     cy.get('input#lastName').type('Cole');
     cy.get('input#password').type('pAssw0rd', { log: false });
     cy.get('select#group').select('driver');
-    cy.fixture('images/photo.jpg').then(photo => {
-        cy.get('input#photo').upload({
-            fileContent: photo,
-            fileName: 'photo.jpg',
-            mimeType: 'application/json'
-        });
-        });
+    cy.get('input#photo').attachFile(filepath)
     cy.get('button').contains('Sign up').click();
     cy.hash().should('eq', '#/log-in');
     });
