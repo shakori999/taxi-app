96b9e4afb32dbdf67de5af54d6f232bcdc7c9295
shakori999 <shakori999@gmail.com>
2022-01-07 17:22:01 +0300

    reorg

--- server/taxi/taxi/settings.py
+++ server/taxi/taxi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-1fsl(&xik5jpyfr!op_h)0jz2%5(1=k7)3!p(4isvx2jdxoiw-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -132,7 +132,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
