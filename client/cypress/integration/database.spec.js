9c1b2bcc102032b36539015f3d31d56d8c0a1628
shakori999 <shakori999@gmail.com>
2022-01-15 04:24:31 +0300

    knex,pg & database queries inserts,read, truncate

--- client/cypress/integration/database.spec.js
+++ client/cypress/integration/database.spec.js
@@ -1,23 +1,21 @@
+const { useIsRTL } = require("react-bootstrap/esm/ThemeProvider");
+
 describe('The database client', function () {
   it('can insert data into a table', function () {
-    cy.task('tableInsert', {
-      table: 'trips_user',
-      rows: [
-        {
-          username: 'test.user@example.com',
-          password: 'pAssw0rd',
-          first_name: 'Test',
-          last_name: 'User',
-          email: 'test.user@example.com',
-          date_joined: new Date(),
-          is_staff: false,
-          is_superuser: false,
-          is_active: true
-        }
-      ],
-      truncate: true
-    }).then((ids) => {
-      cy.wrap(ids).should('have.length', 1);
+    cy.fixture('data/users.json').then((users) => {
+      cy.task('tableInsert', {
+        table: 'trips_user', rows: users, truncate: true
+      }).then((ids) => {
+        cy.wrap(ids).should('have.length', 1);
+      });
     });
+  });
+
+  it('can read data from a table', function() {
+    cy.task('tableSelect', {
+      table: 'trips_user'
+    }).then((users) => {
+      cy.wrap(users).should('have.length', 1);
+    })
   });
 });
--- client/cypress/plugins/index.js
+++ client/cypress/plugins/index.js
@@ -1,22 +1,30 @@
-/// <reference types="cypress" />
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
+const kenx = require('knex');
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  on('task', {
+    async tableInsert ({ table, rows, truncate }) {
+      const client = await kenx({
+        client: 'pg',
+        connection: config.env.database
+      });
+      if (truncate) {
+        await client.raw(`TRUNCATE ${table} RESTART IDENTITY CASCADE`);
+      }
+      return client.insert(rows, ['id']).into(table);
+    },
+    async tableSelect ({ table }) {
+      const client = await kenx({
+        client: 'pg',
+        connection: config.env.database
+      });
+      return client.select().table(table);
+    },
+    async tableTruncate ({ table }) {
+      const client = await kenx({
+        client: 'pg',
+        connection: config.env.database
+      });
+      return client.raw(`TRUNCATE ${table} RESTART IDENTITY CASCADE`);
+    },
+  });
 }
