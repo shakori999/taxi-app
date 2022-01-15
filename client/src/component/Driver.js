379a9f536dd014f19cd599711c9d84c3c707842f
shakori999 <shakori999@gmail.com>
2022-01-15 17:02:07 +0300

    show trips cards for rider/driver

--- client/src/component/Driver.js
+++ client/src/component/Driver.js
@@ -1,40 +1,74 @@
-import React from 'react';
+import React, { useEffect, UseState }from 'react';
 import {
   Breadcrumb, Card, Col, Row
 } from 'react-bootstrap';
 import { Redirect } from 'react-router-dom'; // new
 
+import TripCard from './TripCard';
 import { isDriver } from '../services/AuthService'; // new
+import {getTrips } from '../services/TripService';
 
 function Driver (props) {
+    const [trips, setTrips] = useState([]);
+
+    useEffect(() => {
+        const loadTrips = async () => {
+            const { response, isError } = await getTrips();
+            if (isError) {
+                setTrips([]);
+            } else {
+                setTrips(response.data);
+            }
+        }
+        loadTrips();
+    }, []);
+
     if (!isDriver()) {
         return <Redirect to='/' />
     }
+    const getCurrentTrips = () => {
+        return trips.filter(trip => {
+            return trip.driver !== null && trip.status !== 'COMPLETED';
+        });
+    }
+
+    const getRequestedTrips = () => {
+        return trips.filter(trip => {
+            return trip.status === 'REQUESTED';
+        });
+    }
+
+    const getCompletedTrips = () => {
+        return trips.filter(trip => {
+            return trip.status === 'COMPLETED';
+        });
+    }
+
     return (
         <Row>
         <Col lg={12}>
             <Breadcrumb>
-            <Breadcrumb.Item href='/'>Home</Breadcrumb.Item>
-            <Breadcrumb.Item active>Dashboard</Breadcrumb.Item>
+                <Breadcrumb.Item href='/'>Home</Breadcrumb.Item>
+                <Breadcrumb.Item active>Dashboard</Breadcrumb.Item>
             </Breadcrumb>
-            <Card className='mb-3'>
-            <Card.Header>Current Trip</Card.Header>
-            <Card.Body>
-                No trips.
-            </Card.Body>
-            </Card>
-            <Card className='mb-3'>
-            <Card.Header>Requested Trips</Card.Header>
-            <Card.Body>
-                No trips.
-            </Card.Body>
-            </Card>
-            <Card className='mb-3'>
-            <Card.Header>Recent Trips</Card.Header>
-            <Card.Body>
-                No trips.
-            </Card.Body>
-            </Card>
+            <TripCard
+                title='Current Trips'
+                trips={getCurrentTrips()}
+                group='driver'
+                otherGroup='rider'
+            />
+            <TripCard
+                title='Requested Trips'
+                trips={getRequestedTrips()}
+                group='driver'
+                otherGroup='rider'
+            />
+            <TripCard
+                title='Recent Trips'
+                trips={getCompletedTrips()}
+                group='driver'
+                otherGroup='rider'
+            />
         </Col>
         </Row>
     );
--- client/src/component/Rider.js
+++ client/src/component/Rider.js
@@ -1,35 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState }from 'react';
 import {
   Breadcrumb, Card, Col, Row
 } from 'react-bootstrap';
 import { Redirect } from 'react-router-dom'; // new
 
+import TripCard from './TripCard';
 import { isRider } from '../services/AuthService'; // new
+import { getTrips } from '../services/TripService';
 
 function Rider (props) {
+    const [trips, setTrips] = useState([]);
+
+    useEffect(() => {
+        const loadTrips = async () => {
+            const { responce, isError } = await getTrips();
+            if (isError) {
+                setTrips([]);
+            }else {
+                setTrips(responce.data);
+            }
+        }
+        loadTrips();
+    }, []);
+
+
     if (!isRider()) {
         return <Redirect to='/' />
     }
+
+    const getCurrentTrips = () => {
+        return trips.filter(trip => {
+            return (
+                trip.driver !== null &&
+                trip.status !== 'REQUESTED' &&
+                trip.status !== 'COMPLETED'
+            );
+        });
+    };
+    const getCompletedTrips = () => {
+        return trips.filter(trip => {
+            return trip.status === 'COMPLETED';
+        });
+    };
     return (
         <Row>
-        <Col lg={12}>
-            <Breadcrumb>
-            <Breadcrumb.Item href='/'>Home</Breadcrumb.Item>
-            <Breadcrumb.Item active>Dashboard</Breadcrumb.Item>
-            </Breadcrumb>
-            <Card className='mb-3'>
-            <Card.Header>Current Trip</Card.Header>
-            <Card.Body>
-                No trips.
-            </Card.Body>
-            </Card>
-            <Card className='mb-3'>
-            <Card.Header>Recent Trips</Card.Header>
-            <Card.Body>
-                No trips.
-            </Card.Body>
-            </Card>
-        </Col>
+            <Col lg={12}>
+                <Breadcrumb>
+                    <Breadcrumb.Item href='/'>Home</Breadcrumb.Item>
+                    <Breadcrumb.Item active>Dashboard</Breadcrumb.Item>
+                </Breadcrumb>
+                <TripCard 
+                    title='Current Trip'
+                    trips={getCurrentTrips()}
+                    group='rider'
+                    otherGroup='driver'
+                />
+                <TripCard
+                    title='Recent Trips'
+                    trips={getCompletedTrips()}
+                    group='rider'
+                    otherGroup='driver'
+                />
+            </Col>
         </Row>
     );
 }
