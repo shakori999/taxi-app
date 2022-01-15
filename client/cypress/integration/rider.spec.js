f1ad10e8c6c37c66ce08857e406d00bc96672147
shakori999 <shakori999@gmail.com>
2022-01-15 09:26:25 +0300

    all tests passed: dashboard for rider/driver

--- client/cypress/integration/rider.spec.js
+++ client/cypress/integration/rider.spec.js
@@ -1,21 +1,19 @@
+const logIn = () => {
+  const { username, password } = Cypress.env('rider');
+  cy.server();
+  cy.route('POST', '**/api/log_in/').as('logIn');
+  cy.visit('/#/log-in');
+  cy.get('input#username').type(username);
+  cy.get('input#password').type(password, { log: false });
+  cy.get('button').contains('Log in').click();
+  cy.wait('@logIn');
+};
+
 describe('The rider dashboard', function () {
+
   before(function () {
-    cy.fixture('data/users.json').then((users) => {
-      cy.task('tableInsert', {
-        table: 'trips_user', rows: users, truncate: true
-      })
-    });
-    cy.fixture('data/groups.json').then((groups) => {
-      cy.task('tableInsert', {
-        table: 'auth_group', rows: groups, truncate: true
-      })
-    });
-    cy.fixture('data/user_groups.json').then((groups) => {
-      cy.task('tableInsert', {
-        table: 'trips_user_groups', rows: groups, truncate: true
-      })
-    });
-  })
+    cy.loadUserData();
+  });
 
   it('Cannot be visited if the user is not a rider', function () {
     const { username, password } = Cypress.env('driver')
@@ -38,21 +36,7 @@ describe('The rider dashboard', function () {
   })
 
   it('Can be visited if the user is a rider', function () {
-    const { username, password } = Cypress.env('rider')
-
-    // Capture API calls.
-    cy.server()
-    cy.route('POST', '**/api/log_in/').as('logIn')
-
-    // Log in.
-    cy.visit('/#/log-in')
-    cy.get('input#username').type(username)
-    cy.get('input#password').type(password, { log: false })
-    cy.get('button').contains('Log in').click()
-    cy.hash().should('eq', '#/')
-    cy.get('button').contains('Log out')
-    cy.wait('@logIn')
-
+    logIn();
     cy.visit('/#/rider')
     cy.hash().should('eq', '#/rider')
   })
