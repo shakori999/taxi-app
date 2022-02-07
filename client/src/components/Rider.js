baf6d6b6e9326e68ec50e910ce45cfedfd50b32c
shakori999 <shakori999@gmail.com>
2022-02-07 05:53:59 +0300

    load rider dashboard

--- client/src/components/Rider.js
+++ client/src/components/Rider.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Breadcrumb, Card, Col, Row
 } from 'react-bootstrap';
 
 import { Redirect } from 'react-router-dom'; // new
 
+import TripCard from './TripCard';
 import { isRider } from '../services/AuthService'; // new
+import { getTrips } from '../services/TripService';
+
+const getCurrentTrips = () => {
+    return trips.filter(trip => {
+        return (
+            trip.driver !== null &&
+            trip.status !== 'REQESTED' &&
+            trip.status !== 'COMPLETED'
+        );
+    });
+};
+
+const getCompletedTrips = () => {
+    return trips.filter(trip => {
+        return trip.status === 'COMPLETED';
+    });
+};
 
 function Rider (props) {
+    const [trips, setTrips] = useState([]);
+
+    useEffect(() => {
+        const loadTrips = async () => {
+            const { response, isError } = await getTrips();
+            if (isError) {
+                setTrips([]);
+            } else {
+                setTrips(response.data);
+            }
+        }
+        loadTrips();
+    }, []);
     if (!isRider()) {
         return <Redirect to='/' />
     }
@@ -18,18 +49,18 @@ function Rider (props) {
                     <Breadcrumb.Item href='/'>Home</Breadcrumb.Item>
                     <Breadcrumb.Item active>Dashboard</Breadcrumb.Item>
                 </Breadcrumb>
-                <Card className='mb-3'>
-                    <Card.Header>Current Trip</Card.Header>
-                    <Card.Body>
-                        No trips.
-                    </Card.Body>
-                </Card>
-                <Card className='mb-3'>
-                    <Card.Header>Recent Trips</Card.Header>
-                    <Card.Body>
-                        No trips.
-                    </Card.Body>
-                </Card>
+                <TripCard
+                    title='Current Trip'
+                    trips={getCurrentTrips()}
+                    group='rider'
+                    otherGroup='driver'
+                />
+                <TripCard
+                    title='Recent Trips'
+                    trips={getCompletedTrips()}
+                    group='rider'
+                    otherGroup='driver'
+                />
             </Col>
         </Row>
     );
